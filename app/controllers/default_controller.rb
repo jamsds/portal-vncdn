28b50b672aa7462ecf3a96b3da6a8cd0fca3e4af
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 18:30:41 +0700

    Update Test API

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -2,10 +2,12 @@ class DefaultController < ApplicationController
 	before_action :authenticate_user!
 	
 	def index
-		@requestURI = "/api"
-		response = ApplicationController::SyncProcess.new("#{@requestURI}").getRequest()
-		@response = JSON.parse(response)
-		puts @response
+		if user_signed_in?
+			@requestURI = "/api"
+			response = ApplicationController::SyncProcess.new("#{@requestURI}").getRequest()
+			@response = JSON.parse(response)
+			puts @response
+		end
 	end
 
 	def api
