8f656ca6c8287f81b3e781c85d96a3d1e7f2dc1d
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 19:32:27 +0700

    Update Reseller

---
+++ app/controllers/reseller_controller.rb
@@ -0,0 +1,7 @@
+class ResellerController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user_child = User.where(parent_uuid: current_user.username)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # Account Management
   get '/account', to: "account#index"
 
+  # Reseller Management
+  get '/reseller', to: "reseller#index"
+
   # Authenticator
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords", confirmations: 'users/confirmations' }
 end
