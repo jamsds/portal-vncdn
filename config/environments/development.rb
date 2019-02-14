5bd040cd7c6ebd19b839dbc9db882ddddcd1de6d
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 19:09:29 +0700

    Update Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Devise Mailer
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'id.demo.vncdn.vn' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
