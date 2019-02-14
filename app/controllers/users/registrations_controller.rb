d1b3dc6162bc67730feebd76d6eabcde52423a9f
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 23:08:53 +0700

    Update Devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
    	def sign_up_params
      	params.require(:user).permit(:name, :username, :email, :uuid, :parent_uuid, :password, :password_confirmation) if params[:user].present?
    	end
+
+		def after_inactive_sign_up_path_for(resource)
+			new_user_confirmation_path
+		end
 end
