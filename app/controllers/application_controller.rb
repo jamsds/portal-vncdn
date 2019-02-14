eca5eeeac2b26b057a9080d6c9f59a4fff7a99e2
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 22:29:03 +0700

    Update API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,26 @@ class ApplicationController < ActionController::Base
 	$ACCESS_KEY_SECRET = 'M6yji4lILlMz9E9zd869YyG7pf1Q811a'
 	$X_SPD_NONCE = rand(10000..99999)
 	$ROOT_ID = "30133"
+
+	class SyncProcess
+    def initialize(requestURI)
+      @requestURI = requestURI
+    end
+
+    def postRequest
+      url = URI("http://127.0.0.1:3000"+@requestURI)
+
+      http = Net::HTTP.new(url.host, url.port)
+      request = Net::HTTP::Post.new(url)
+      response = http.request(request).read_body
+    end
+
+    def getRequest
+      url = URI("http://127.0.0.1:3000"+@requestURI)
+
+      http = Net::HTTP.new(url.host, url.port)
+      request = Net::HTTP::Get.new(url)
+      response = http.request(request).read_body
+    end
+  end
 end
