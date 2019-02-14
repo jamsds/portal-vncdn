5c91bca68483103ff899552762f2f82daed018ad
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 18:31:56 +0700

    Update Devise

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_180000) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
