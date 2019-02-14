c02ea4d3635ed9c92a27255326a4e4ffa1232ba3
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 22:42:10 +0700

    Update API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,6 +13,34 @@ class ApiController < ApplicationController
 	before_action :base_endpoint, only: [:createCustomer]
 	before_action :cdn_endpoint, only: [:listDomain]
 
+	# Swiftfederation Config
+	$ACCESS_KEY_ID = '0cavp8cG1vd149Oy'
+	$ACCESS_KEY_SECRET = 'M6yji4lILlMz9E9zd869YyG7pf1Q811a'
+	$X_SPD_NONCE = rand(10000..99999)
+	$ROOT_ID = "30133"
+
+	class SyncProcess
+    def initialize(requestURI)
+      @requestURI = requestURI
+    end
+
+    def postRequest
+      url = URI("http://id.demo.vncdn.vn"+@requestURI)
+
+      http = Net::HTTP.new(url.host, url.port)
+      request = Net::HTTP::Post.new(url)
+      response = http.request(request).read_body
+    end
+
+    def getRequest
+      url = URI("http://id.demo.vncdn.vn"+@requestURI)
+
+      http = Net::HTTP.new(url.host, url.port)
+      request = Net::HTTP::Get.new(url)
+      response = http.request(request).read_body
+    end
+  end
+
 	# Generate signatureAlgorithm
 	class Algorithm
 		def initialize(requestMethod, requestURI, requestBody)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,32 +2,4 @@ class ApplicationController < ActionController::Base
 	require "uri"
   require "net/http"
   require "date"
-
-  # Swiftfederation Config
-	$ACCESS_KEY_ID = '0cavp8cG1vd149Oy'
-	$ACCESS_KEY_SECRET = 'M6yji4lILlMz9E9zd869YyG7pf1Q811a'
-	$X_SPD_NONCE = rand(10000..99999)
-	$ROOT_ID = "30133"
-
-	class SyncProcess
-    def initialize(requestURI)
-      @requestURI = requestURI
-    end
-
-    def postRequest
-      url = URI("http://id.demo.vncdn.vn"+@requestURI)
-
-      http = Net::HTTP.new(url.host, url.port)
-      request = Net::HTTP::Post.new(url)
-      response = http.request(request).read_body
-    end
-
-    def getRequest
-      url = URI("http://id.demo.vncdn.vn"+@requestURI)
-
-      http = Net::HTTP.new(url.host, url.port)
-      request = Net::HTTP::Get.new(url)
-      response = http.request(request).read_body
-    end
-  end
 end
