d8a009b1d61c26b7ab6d4e61e2209a138133ae35
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-14 18:41:24 +0700

    Test Devise

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,16 +1,3 @@
 class DefaultController < ApplicationController
 	before_action :authenticate_user!
-	
-	def index
-		if user_signed_in?
-			@requestURI = "/api"
-			response = ApplicationController::SyncProcess.new("#{@requestURI}").getRequest()
-			@response = JSON.parse(response)
-			puts @response
-		end
-	end
-
-	def api
-		render :json => { message: "success" }.to_json
-	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Devise Mailer
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
