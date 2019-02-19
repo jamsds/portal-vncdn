e9049b80e76419b27904f21e4c3be8c282856a0f
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-19 19:47:26 +0700

    Disable Email Confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
 	# Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :confirmable, :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+  #      :confirmable,
 
   has_one :credit, 			 dependent: :destroy
   has_one :subscription, dependent: :destroy
