b287dfb90ec81990f63772fdbd8326e94f6e57d1
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-20 11:26:15 +0700

    Update Sidekiq

--- app/workers/usage_bandwidth.rb
+++ app/workers/usage_bandwidth.rb
@@ -6,45 +6,53 @@ class UsageBandwidth
   def perform
   	@thisMonth = Date.today.strftime("%Y-%m")
 
-  	# endTime = Time.now.utc
-    # @endTime = (endTime - 20*60).strftime("%Y-%m-%dT%H:%M:00Z")
-    @endTime = "2019-02-20T04:00:00Z"
+  	endTime = Time.now.utc
+    @endTime = (endTime - 20*60).strftime("%Y-%m-%dT%H:%M:00Z")
+    # @endTime = "2019-02-20T04:00:00Z"
 
-  	Subscription.where(status: 1).each do |subscription|
-      @user = User.find(subscription.user_id)
+		# Run if subscription status is active
+		Subscription.where(status: 1).each do |subscription|
+			# Find user with subscription is active
+			@user = User.find(subscription.user_id)
 
-      # if @user.bandwidths.find_by(monthly: @thisMonth).present?
-      #   startTime = @user.bandwidths.find_by(monthly: @thisMonth).updated_at
-      # else
-      #   startTime = endTime - 20*60
-      # end
+			# Create bandwidth record if user is new user
+			if !@user.bandwidths.where(monthly: @thisMonth).present?
+				@user.bandwidths.create(monthly: @thisMonth)
+			end
 
-      # @startTime = startTime.strftime("%Y-%m-%dT%H:%M:00Z")
-      @startTime = "2019-02-20T00:00:00Z"
+			# Set start time is last update
+			if @user.bandwidths.find_by(monthly: @thisMonth).present?
+				startTime = @user.bandwidths.find_by(monthly: @thisMonth).updated_at
+			else
+				startTime = endTime - 20*60
+			end
 
-      @listDomain = []
+			@startTime = startTime.strftime("%Y-%m-%dT%H:%M:00Z")
+			# @startTime = "2019-02-20T00:00:00Z"
 
-      @requestURI = "/api/v1.1/customerDelivery/?id=#{@user.uuid}"
-      @deliverys = JSON.parse(ApplicationController::SyncProcess.new("#{@requestURI}").getRequest())
+			@listDomain = []
 
-      @deliverys.each do |delivery|
+			@requestURI = "/api/v1.1/customerDelivery/?id=#{@user.uuid}"
+			@deliverys = JSON.parse(ApplicationController::SyncProcess.new("#{@requestURI}").getRequest())
+
+			@deliverys.each do |delivery|
 				@listDomain << delivery
 			end
 
-      @requestURI = "/api/v1.1/customerDownload/?id=#{@user.uuid}"
-      @downloads = JSON.parse(ApplicationController::SyncProcess.new("#{@requestURI}").getRequest())
+			@requestURI = "/api/v1.1/customerDownload/?id=#{@user.uuid}"
+			@downloads = JSON.parse(ApplicationController::SyncProcess.new("#{@requestURI}").getRequest())
 
-      @downloads.each do |download|
+			@downloads.each do |download|
 				@listDomain << download
 			end
 
 			@listDomain.each do |domain|
 				# puts domain["name"]
 				@requestURI = "/api/v1.1/customerVolume/?domain="+domain["name"]+"&startTime="+@startTime+"&endTime="+@endTime
-        @bandwidth = JSON.parse(ApplicationController::SyncProcess.new("#{@requestURI}").postRequest())
+				@bandwidth = JSON.parse(ApplicationController::SyncProcess.new("#{@requestURI}").postRequest())
 
-        puts @bandwidth
+				puts @bandwidth
 			end
-    end
+		end
   end
 end
