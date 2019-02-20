fe2811e1e5cdf1548ce6de74d83c138facf6ebf8
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-20 14:53:25 +0700

    Update Sidekiq

--- app/workers/usage_bandwidth.rb
+++ app/workers/usage_bandwidth.rb
@@ -4,12 +4,17 @@ class UsageBandwidth
   include Sidekiq::Worker
 
   def perform
+  	# Concept
+    ## Bandwidth usage will calculate every 0,15,30,45 minute of an hour, every bandwidth used will generate a cash log for customer follow what their use on a day
+    ## monthly data for report
   	@thisMonth = Date.today.strftime("%Y-%m")
 
+  	## Time to generate the log of Swiftfederation slow more 20 minutes than real-time, so startTime needs to subtract 20 minutes
+    ## Time.now using 20 minutes with number*second
   	endTime = Time.now.utc
     @endTime = (endTime - 20*60).strftime("%Y-%m-%dT%H:%M:00Z")
-    # @endTime = "2019-02-20T04:00:00Z"
 
+    # Last update subtract 5 minutes because we need delay to make sure data on Swiftfederation update complete
     @last_update = (endTime - 15*60).strftime("%Y-%m-%dT%H:%M:00Z")
 
 		# Run if subscription status is active
@@ -17,20 +22,26 @@ class UsageBandwidth
 			# Find user with subscription is active
 			@user = User.find(subscription.user_id)
 
-			# Create bandwidth record if user is new user
+			# Because every bandwidth log will be generated once time per month,
+      # so we need to a defining month of logs, if it exists we don't need to create new
+      # puts user.bandwidths.where(monthly: @thisMonth).present?
 			if !@user.bandwidths.where(monthly: @thisMonth).present?
 				@user.bandwidths.create(monthly: @thisMonth, last_update: @last_update)
 			end
 
 			# Set start time is last update
-			if @user.bandwidths.find_by(monthly: @thisMonth).present?
-				startTime = @user.bandwidths.find_by(monthly: @thisMonth).last_update
+			if @user.bandwidths.find_by(monthly: @thisMonth).last_update.present?
+				@startTime = @user.bandwidths.find_by(monthly: @thisMonth).last_update.strftime("%Y-%m-%dT%H:%M:00Z")
 			else
-				startTime = endTime - 20*60
+				@startTime = (endTime - 20*60).strftime("%Y-%m-%dT%H:%M:00Z")
 			end
 
-			@startTime = startTime.strftime("%Y-%m-%dT%H:%M:00Z")
+			# Time format for testing
 			# @startTime = "2019-02-20T00:00:00Z"
+			# @endTime = "2019-02-20T04:00:00Z"
+
+			# Instead, each log is bandwidth update, a last_update record will be updated time of last sidekiq run
+      @user.bandwidths.find_by(monthly: @thisMonth).update(last_update: @last_update)
 
 			@listDomain = []
 
@@ -55,9 +66,12 @@ class UsageBandwidth
 
 				@bandwidths.each do |bandwidth|
 					bandwidth["volumes"].each do |value|
+						# Test
 						puts value["value"]
-						
-						@user.bandwidths.find_by(monthly: @thisMonth, last_update: @last_update).increment! :bandwidth_usage, value["value"]/1000.00
+
+						# Data save on database be converted MB for decrease the decimal string length
+						# Update new bandwidth value by increase data of bandwidth log
+						@user.bandwidths.find_by(monthly: @thisMonth).increment! :bandwidth_usage, value["value"]/1000.00
 					end
 				end
 			end
