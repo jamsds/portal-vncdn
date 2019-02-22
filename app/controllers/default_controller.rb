ddc795b2c76c3e83384f87c5692bafaa9007ef8b
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-22 22:43:44 +0700

    Update Portal

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -20,5 +20,38 @@ class DefaultController < ApplicationController
 		if !current_user.notification.present?
 			Notification.create(user_id: current_user.id)
 		end
+
+		@startDateOfThisMonth = Date.current.beginning_of_month.strftime("%B %d")
+		@dateTodayOfThisMonth = Date.current.strftime("%d, %Y")
+
+		@thisMonth = Date.today.strftime("%Y-%m")
+
+    if current_user.credit.transactions.nil? || current_user.credit.transactions.where(monthly: @thisMonth, transaction_type: "Automatic Payment", status: "succeeded").present?
+      @totalPrice = 0
+      @threshold = 0
+    elsif current_user.subscription.nil?
+      @totalPrice = 0
+      @threshold = 0
+    else
+      bwdPrice = current_user.subscription.bwd_price
+      stgPrice = current_user.subscription.stg_price
+
+      if current_user.bandwidths.present?
+        bwdUsage = current_user.bandwidths.find_by(monthly: @thisMonth).bandwidth_usage / 1000000.00
+      else
+        bwdUsage = 0
+      end
+
+      if current_user.storages.present?
+        stgUsage = current_user.storages.find_by(monthly: @thisMonth).storage_usage / 1000000.00
+      else
+        stgUsage = 0
+      end
+
+      totalPrice = (stgPrice * stgUsage) + (bwdPrice * bwdUsage)
+
+      @totalPrice = totalPrice
+      @threshold = (totalPrice/1000000.00) * 100
+    end
 	end
 end
