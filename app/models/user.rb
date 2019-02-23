ba122fd7abdd7a97a56f47db007bbfc9143c7c3c
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-23 17:52:08 +0700

    Update Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 	# Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
          :confirmable,
 
   has_one :credit, 			 dependent: :destroy
