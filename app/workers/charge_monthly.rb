5b16e5fa62dfe5002a8169bbadff96052b05a83e
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-23 12:58:09 +0700

    Update Sidekiq

--- app/workers/charge_monthly.rb
+++ app/workers/charge_monthly.rb
@@ -3,47 +3,98 @@ require 'sidekiq-scheduler'
 class ChargeMonthly
   include Sidekiq::Worker
 
+  ## Run on first day of the month, it mean we need find data on previous month to make payment
   def perform
-  	# Defind usage on this month
-  	@thisMonth = Date.today.strftime("%Y-%m")
+    @previousMonth = (Date.today - 1.months).strftime("%Y-%m")
 
-  	# Run if subscription status is active
+  	# Only exec with subscription status is active
 		Subscription.where(status: 1).each do |subscription|
-			# Find user with subscription is active
+
+			# Find user
 			@user = User.find(subscription.user_id)
+
+      # Get bandwidth & storage usage on previous month
+      bwdUsage = @user.bandwidths.find_by(monthly: @previousMonth).bandwidth_usage * 1000.00
+      stgUsage = @user.storages.find_by(monthly: @previousMonth).storage_usage * 1000.00
+
+      # Calculator usage pricing
+      @totalPrice = (@user.subscription.stg_price * (stgUsage / 1000000000.00)) + (@user.subscription.bwd_price * (bwdUsage / 1000000000.00))
+
+      @description = "Delivery Appliance running in global: #{ApplicationController::FormatNumber.new(bwdUsage).formatHumanSize()}, and File Appliance in global: #{ApplicationController::FormatNumber.new(stgUsage).formatHumanSize()} (Source:#{Package.find(@user.subscription.package).name} [#{@user.subscription.name}])"
 			
-      # Effect only with subscription type automatic payment
-      if subscription.payment_type == 2
-  			# Get bandwidth & storage usage on this month
-  			bwdUsage = @user.bandwidths.find_by(monthly: @thisMonth).bandwidth_usage * 1000.00
-        stgUsage = @user.storages.find_by(monthly: @thisMonth).storage_usage * 1000.00
+      # Effect only with subscription type automatic payment by credit card
+      if @user.subscription.payment_type == 2 && @user.credit.card_token.present?
+        charge = Stripe::Charge.create(
+          :amount => @totalPrice.to_i,
+          :currency => "vnd",
+          :customer => @user.credit.stripe_token,
+          :source => @user.credit.card_token,
+          :description => @description
+        )
 
-        # Calculator Pricing
-        totalPrice = (@user.subscription.stg_price * (stgUsage / 1000000000.00)) + (@user.subscription.bwd_price * (bwdUsage / 1000000000.00))
+        # Defind charge succeeded response from Stripe
+        if charge["status"] == "succeeded"
 
-        # Check Credit
-        if @user.credit.credit_value != 0 && @user.credit.credit_value > totalPrice
-          @user.credit.decrement! :credit_value, totalPrice
+          # Create transaction succeeded
+          @user.credit.transactions.create(
+            description: @description,
+            transaction_type: 'Automatic Payment',
+            stripe_id: charge["id"],
+            amount: @totalPrice.to_i,
+            card_id: @user.credit.card_token,
+            card_name: @user.credit.card_name,
+            card_number: @user.credit.last4,
+            card_brand: @user.credit.card_brand,
+            status: 'succeeded',
+            monthly: @previousMonth
+          )
+        end
+      end
+
+      # Effect only with subscription type automatic payment by deposit
+      if @user.subscription.payment_type == 2 && @user.credit.card_token.nil?
+
+        # Check credit
+        if @user.credit.credit_value != 0 && @user.credit.credit_value > @totalPrice
+
+          # Update credit balance
+          @user.credit.decrement! :credit_value, @totalPrice
           @status = 'succeeded'
         else
           @status = 'failed'
 
-          # Suspend Subscription
+          # Suspend subscription if payment failed
           @user.subscription.update(status: 2)
         end
 
         # Create transaction of this month
         @user.credit.transactions.create(
-          description: "Delivery Appliance running in global: #{ApplicationController::FormatNumber.new(bwdUsage).formatHumanSize()}, and File Appliance in global: #{ApplicationController::FormatNumber.new(stgUsage).formatHumanSize()} (Source:#{Package.find(@user.subscription.package).name} [#{@user.subscription.name}])",
+          description: @description,
           transaction_type: 'Automatic Payment',
-          amount: totalPrice,
+          amount: @totalPrice,
           status: @status,
-          monthly: @thisMonth
+          monthly: @previousMonth
         )
-      # Effect only with subscription type monthly
-      elsif subscription.payment_type == 3
-
       end
 		end
+
+    # Rescue error and create transaction failed with message
+    rescue Stripe::CardError => e
+
+      # Create transaction failed
+      @user.credit.transactions.create(
+        description: e.message,
+        transaction_type: 'Automatic Payment',
+        amount: @totalPrice.to_i,
+        card_id: @user.credit.card_token,
+        card_name: @user.credit.card_name,
+        card_number: @user.credit.last4,
+        card_brand: @user.credit.card_brand,
+        status: 'failed',
+        monthly: @previousMonth
+      )
+
+      # Suspend subscription if payment failed
+      @user.subscription.update(status: 2)
   end
 end
