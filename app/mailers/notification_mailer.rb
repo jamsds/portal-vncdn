8b0629eed5d82384f60df6aa987104c0a113f7ca
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-26 17:03:27 +0700

    Test Devise

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -27,6 +27,8 @@ class NotificationMailer < ApplicationMailer
 
     @domain = User.find_by(username: @user.parent_uuid).domain
 
+    ActionMailer::Base.default_url_options[:host] pointing to @domain
+
     @card_brand = @user.credit.card_brand
     @card_number = @user.credit.last4
     @expires = @user.credit.expires
