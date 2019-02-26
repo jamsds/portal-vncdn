b7db960618a44f3cb46fcfa1349607d1d8dcc4b4
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-26 14:50:15 +0700

    Update Devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
 	def create
 		# Verify user reseller owner
-		@user = User.find_by(email: sign_in_params["email"]).parent_uuid
-		@parent_uuid = User.find_by(username: @user).domain
+		if User.find_by(email: sign_in_params["email"]).accountType == 1
+			@parent = User.find_by(email: sign_in_params["email"]).parent_uuid
+			@parent_uuid = User.find_by(username: @parent)
+		elsif User.find_by(email: sign_in_params["email"]).accountType == 2
+			@parent_uuid = User.find_by(email: sign_in_params["email"])
+		end
 
-		if @parent_uuid == request.host
+		if @parent_uuid.domain == request.host
  			self.resource = warden.authenticate!(auth_options)
  			set_flash_message!(:notice, :signed_in)
 	    sign_in(resource_name, resource)
