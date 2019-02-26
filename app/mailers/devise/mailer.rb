8637d9642aa01b0ba5bc3eed2f37a69324fed270
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-26 17:44:32 +0700

    Test Devise

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -6,7 +6,7 @@ if defined?(ActionMailer)
 
     def confirmation_instructions(record, token, opts={})
       @token = token
-      @host = request.host_with_port
+      @host = 'abc.com'
       devise_mail(record, :confirmation_instructions, opts)
     end
 
