c96e7a72a3642014aa285bfd1664582f8ca19168
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-27 19:46:35 +0700

    Update Devise

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,3 +1,10 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
 	layout 'signin'
+
+	before_action :clear_cookies
+
+  private
+  	def clear_cookies
+  		cookies.signed["_ssid"] = nil
+  	end
 end
