8e4aa9c099f5806383326bba78079caa07ccc5ea
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-27 16:52:35 +0700

    Update Devise

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -7,28 +7,73 @@ if defined?(ActionMailer)
     def confirmation_instructions(record, token, opts={})
       @token = token
       @host = Thread.current[:request_host]
+
+      if User.find_by(domain: @host).present?
+        @color = User.find_by(domain: @host).color
+        @logo = User.find_by(domain: @host).logo
+      else
+        @color = "#f68100"
+        @logo = "http://<%= @host %>/assets/logo_dark.png"
+      end
+
       devise_mail(record, :confirmation_instructions, opts)
     end
 
     def reset_password_instructions(record, token, opts={})
       @token = token
       @host = Thread.current[:request_host]
+
+      if User.find_by(domain: @host).present?
+        @color = User.find_by(domain: @host).color
+        @logo = User.find_by(domain: @host).logo
+      else
+        @color = "#f68100"
+        @logo = "http://reseller.vncdn.vn/assets/logo_dark.png"
+      end
+
       devise_mail(record, :reset_password_instructions, opts)
     end
 
     def unlock_instructions(record, token, opts={})
       @token = token
       @host = Thread.current[:request_host]
+
+      if User.find_by(domain: @host).present?
+        @color = User.find_by(domain: @host).color
+        @logo = User.find_by(domain: @host).logo
+      else
+        @color = "#f68100"
+        @logo = "http://reseller.vncdn.vn/assets/logo_dark.png"
+      end
+
       devise_mail(record, :unlock_instructions, opts)
     end
 
     def email_changed(record, opts={})
       @host = Thread.current[:request_host]
+
+      if User.find_by(domain: @host).present?
+        @color = User.find_by(domain: @host).color
+        @logo = User.find_by(domain: @host).logo
+      else
+        @color = "#f68100"
+        @logo = "http://reseller.vncdn.vn/assets/logo_dark.png"
+      end
+
       devise_mail(record, :email_changed, opts)
     end
 
     def password_change(record, opts={})
       @host = Thread.current[:request_host]
+
+      if User.find_by(domain: @host).present?
+        @color = User.find_by(domain: @host).color
+        @logo = User.find_by(domain: @host).logo
+      else
+        @color = "#f68100"
+        @logo = "http://reseller.vncdn.vn/assets/logo_dark.png"
+      end
+
       devise_mail(record, :password_change, opts)
     end
   end
