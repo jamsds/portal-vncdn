ccf5ea42f4b52a779d346fb3e876e2491e53dff2
tho.nguyen <tho.nguyen@vnetwork.vn>
2019-02-27 19:04:47 +0700

    Update Notifications

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,6 @@ class NotificationMailer < ApplicationMailer
   default from: 'no-reply@vncdn.vn'
   layout 'mailer'
 
-  before_action :set_variable_mailer
-
   class SetVariable
     def initialize(id)
       @id = id
@@ -14,26 +12,34 @@ class NotificationMailer < ApplicationMailer
 
       if @user.parent_uuid.present? && User.find_by(username: @user.parent_uuid).domain.present?
         @host = User.find_by(username: @user.parent_uuid).domain
+        puts @host
       else
         @host = 'reseller.vncdn.vn'
+        puts @host
       end
 
       if @user.parent_uuid.present? && User.find_by(username: @user.parent_uuid).color.present?
         @color = User.find_by(username: @user.parent_uuid).color
+        puts @color
       else
         @color = "#f68100"
+        puts @color
       end
 
       if @user.parent_uuid.present? && User.find_by(username: @user.parent_uuid).company.present?
         @company = User.find_by(username: @user.parent_uuid).company
+        puts @company
       else
         @company = "VNCDN"
+        puts @company
       end
 
       if @user.parent_uuid.present? && User.find_by(username: @user.parent_uuid).logo.present?
-        @company = User.find_by(username: @user.parent_uuid).logo
+        @logo = User.find_by(username: @user.parent_uuid).logo
+        puts @logo
       else
-        @company = "http://reseller.vncdn.vn/assets/logo_dark.png"
+        @logo = "http://reseller.vncdn.vn/assets/logo_dark.png"
+        puts @logo
       end
     end
   end
@@ -45,6 +51,8 @@ class NotificationMailer < ApplicationMailer
 
     @name = @user.name
     @email = @user.email
+
+    SetVariable.new(id).variableBuild()
 
     @bwdLimit = @user.subscription.bwd_limit
     @stgLimit   = @user.subscription.stg_limit
@@ -62,6 +70,8 @@ class NotificationMailer < ApplicationMailer
     @name = @user.name
     @email = @user.email
 
+    SetVariable.new(id).variableBuild()
+
     @card_brand = @user.credit.card_brand
     @card_number = @user.credit.last4
     @expires = @user.credit.expires
@@ -72,9 +82,4 @@ class NotificationMailer < ApplicationMailer
       subject: "VNCDN - Card Linked Notification",
     )
   end
-
-  private
-    def set_variable_mailer
-      SetVariable.new(id).variableBuild()
-    end
 end
